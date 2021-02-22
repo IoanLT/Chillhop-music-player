3b05805a841560b612db15aa61a2ca0aadb53a4f
IoanLT <ioan.tranole@gmail.com>
2021-02-22 17:39:27 +0000

    Created song and player components

---
+++ src/components/Player.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Player() {
+    return (
+        <div className="player">
+            <div className="time-control">
+                <p>Start Time</p>
+                <input type="range" />
+                <p>End Time</p>
+            </div> 
+            <div className="play-control">
+                
+            </div>          
+        </div>
+    )
+}
---
+++ src/components/Song.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Song() {
+    return (
+        <div className="song-wrapper">
+            <h1>Picture</h1>
+            <h1>Song name</h1>
+            <h1>Artist</h1>
+        </div>
+    )
+}
